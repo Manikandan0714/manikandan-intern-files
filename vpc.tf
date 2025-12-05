48fa49f01ea8192054279e4d888e06c4f8d2157c
Manikandan0714 <manikandansethuraman007@gmail.com>
2025-12-05 11:32:38 +0530

    Initial VPC setup

---
+++ vpc.tf
@@ -0,0 +1,107 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+# Subnet
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+# Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "public-rt"
+  }
+}
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+# Security Group
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow HTTP and SSH"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "web-sg"
+  }
+  }
+# Allow HTTP
+resource "aws_vpc_security_group_ingress_rule" "http" {
+  security_group_id = aws_security_group.web_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+# Allow SSH
+resource "aws_vpc_security_group_ingress_rule" "ssh" {
+  security_group_id = aws_security_group.web_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+# Allow all outbound traffic
+resource "aws_vpc_security_group_egress_rule" "all_out" {
+  security_group_id = aws_security_group.web_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+# EC2 Instance
+resource "aws_instance" "web" {
+  ami                    = "ami-0ecb62995f68bb549"
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  key_name               = "keyus"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install apache2 -y
+              systemctl start apache2
+              systemctl enable apache2
+              echo "<h1>Welcome to Manikandan's Web Server</h1>" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "Manikandan-Web"
+  }
+}
+# Output Public IP
+output "public_ip" {
+  value = aws_instance.web.public_ip
+}
+
